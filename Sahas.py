07cb9ba02495370104286c860bb3c80457f0ef56
aryan-hacker-dragon <137382838+aryan-hacker-dragon@users.noreply.github.com>
2024-03-13 22:21:47 +0530

    Update Sahas.py

--- Sahas.py
+++ Sahas.py
@@ -32,7 +32,7 @@ Cheers to you, Sahas! 🌟""")
     
     st.markdown("### 🎂 Let's Celebrate! 🥳")
 if st.button("Reload Image"):
-    st.experimental_rerun()
+    st.rerun()
     # # Text input for user's birthday message
     # birthday_message = st.text_area("Your Birthday Message:", max_chars=280)
 
